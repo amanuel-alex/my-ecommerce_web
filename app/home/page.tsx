fe134c09455aef99b67f8172c82e7238513afc19
amanuel <amanuel2123alex@gmail.com>
2024-12-24 10:27:47 +0300

    shadcn/ui set up

---
+++ app/home/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+const HomePage = () => {
+  return <Button variant="secondary">HomePage</Button>;
+};
+
+export default HomePage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import StartingPage from "./components/page";
+import HomePage from "./home/page";
 export default function Home() {
   return (
     <div>
       <StartingPage />
+      <HomePage />
     </div>
   );
 }
