c4c2a1763ec4febd5e834df53c167b884a9fa2a2
amanuel <amanuel2123alex@gmail.com>
2024-12-24 09:37:11 +0300

    adjusting navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-
+import { FaSearch } from "react-icons/fa";
+import { RiMenuUnfoldFill } from "react-icons/ri";
 const NavbarContainer = () => {
   return (
     <nav className=" text-white  ">
@@ -23,10 +24,16 @@ const NavbarContainer = () => {
             Column 2
           </div>
 
-          <div className="col-span-1 relative flex items-center">
-            <div className="absolute left-0 mx-1 rounded-sm ">
-              <select className="bg-white border border-gray-300  p-2 text-gray-700">
+          <div className="col-span-1 relative flex items-center ">
+            <div className="absolute left-0 ml-1  border rounded focus:border-orange-700 focus:ring-red-500-  ">
+              <select className="bg-white text-slate-400  focus:outline-none  border       py-[10px] ">
                 <option value="">ALL</option>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
                 <option value="option1">Option 1</option>
                 <option value="option2">Option 2</option>
                 <option value="option3">Option 3</option>
@@ -35,25 +42,14 @@ const NavbarContainer = () => {
             <input
               type="text"
               id="search"
-              placeholder="Search..."
-              className="w-full py-2 pl-12 pr-12 rounded-md  border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500 text-gray-700"
+              placeholder="Search Amazon..."
+              className="w-full p-2  pl-28 pr-12 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-red-500 text-gray-700 rounded-lg"
             />
 
-            <div className="absolute right-0 mx-1 rounded-sm ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="absolute top-1/2 right-3 transform -translate-y-1/2 h-5 w-5 text-gray-500 bg-slate-300"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M11 4a7 7 0 011.5 13.8m2.56 2.54l4.25 4.25m0-4.25a7 7 0 10-9.5-9.5"
-                ></path>
-              </svg>
+            <div className="absolute right-0 ml-1 rounded-sm ">
+              <div className="bg-red-700 border-gray-300  p-3 text-gray-700 hover:bg-red-800 rounded-r">
+                <FaSearch color="white" />
+              </div>
             </div>
           </div>
 
@@ -71,6 +67,17 @@ const NavbarContainer = () => {
 
           <div className="rounded-md border border-gray-300 hover:ring-green-500 text-white p-2">
             Column 7
+          </div>
+        </div>
+      </div>
+      <div className="max-w-8xl bg-green-700   ">
+        <div className="grid grid-cols-[3rem_1fr]  ">
+          <div className="  focus:border-gray-300 focus:ring-2 focus:ring-blue-500 rounded-md  text-white p-2 ">
+            <RiMenuUnfoldFill className="text-3xl" />
+          </div>
+
+          <div className="rounded-md  hover:ring-green-500 text-white p-2">
+            Column
           </div>
         </div>
       </div>
